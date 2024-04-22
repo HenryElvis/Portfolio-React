50994fba44219fffd50f443d65b5414d7f0240a4
Elvis <elvishenry2402@gmail.com>
2024-04-22 16:53:22 +0200

    Animation title on focus

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,38 @@
+import { useRef, useEffect, RefObject } from 'react';
+
 import '../styles/header/header.scss';
 
 const Header = () =>
 {
+    const titleRef = useRef<HTMLElement | null>(null);
+
+    useEffect(() => {
+
+        function AnimationOnFocus()
+        {
+            if (titleRef.current === null) return;
+            
+            if (document.hasFocus())
+                titleRef.current.style.animationPlayState = "running";
+            else
+                titleRef.current.style.animationPlayState = "paused";
+        }
+
+        AnimationOnFocus();
+
+        window.addEventListener('focus', AnimationOnFocus);
+        window.addEventListener('blur', AnimationOnFocus);
+
+        return () => {
+            window.removeEventListener('focus', AnimationOnFocus);
+            window.removeEventListener('blur', AnimationOnFocus);
+        }
+
+    }, []);
+
     return (
         <section id="title">
-            <h1 translate="no" id="titleAnim">P1RTF1L!1</h1>
+            <h1 ref={titleRef as RefObject<HTMLHeadingElement>} translate="no">P1RTF1L!1</h1>
             <p>12222222222</p>
 
             <button id="cmd-toggle-btn">
