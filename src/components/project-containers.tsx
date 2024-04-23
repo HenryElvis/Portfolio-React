5c6e2d68d23a66c388f24328ece4565ce32851ea
Elvis Henry <elvishenry2402@gmail.com>
2024-04-23 15:18:18 +0200

    add image

--- src/components/project-containers.tsx
+++ src/components/project-containers.tsx
@@ -8,6 +8,10 @@ const ProjectContainers = () => {
             <ProjectItem index={1} />
             <ProjectItem index={2} />
             <ProjectItem index={3} />
+            <ProjectItem index={4} />
+            <ProjectItem index={5} />
+            <ProjectItem index={6} />
+            <ProjectItem index={7} />
         </div>
     )
 }
