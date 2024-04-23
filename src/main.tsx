5c6649cfd4b89a9b0522ef8dac69fe8eec118de4
Elvis <elvishenry2402@gmail.com>
2024-04-23 23:25:09 +0200

    testing router

--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,17 @@ import Header from './components/header'
 import CardContainers from './components/card-containers'
 import Footer from './components/footer'
 import ProjectContainers from './components/project-containers'
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('header')!).render(
   <React.StrictMode>
     <Header />
+  </React.StrictMode>,
+)
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
   </React.StrictMode>,
 )
 
---
+++ src/scripts/Router.tsx
@@ -0,0 +1,14 @@
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <div>Acceuil</div>
+//     },
+//     {
+//         path: "/about",
+//         element: <div>About</div>
+//     }
+// ]);
+
+// export default router;
