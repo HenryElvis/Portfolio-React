a700da83254964e180980fde3e525439e1b95478
Elvis Henry <elvishenry2402@gmail.com>
2024-04-24 14:18:41 +0200

    cmd state log on console

--- src/components/cmd.tsx
+++ src/components/cmd.tsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from 'react';
 import '../styles/header/cmd.scss';
 
 const Cmd = () => {
+    const [clicked, setClicked] = useState(false);
+    const [cmdHide, setCmdHide] = useState(true);
+
+    useEffect(() => {
+        const HandleButton = () => {
+
+            if (clicked) return;
+            
+            setClicked(true);
+            setCmdHide(!cmdHide);
+
+            setTimeout(() => {
+                setClicked(false);
+            }, 500);
+
+            if (cmdHide)
+            {
+                console.log("cmd show");
+            }
+            else
+            {
+                console.log("cmd hide");
+            }
+        };
+
+        const button = document.querySelector('.cmd-toggle-btn');
+        button?.addEventListener('click', HandleButton);
+
+        return () => {
+            button?.removeEventListener('click', HandleButton);
+        };
+    }, [clicked, cmdHide]);
 
     return (
         <button className="cmd-toggle-btn">
