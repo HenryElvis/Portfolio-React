c72de80a0ed50c91130f9218176f87ec826687ea
Elvis <elvishenry2402@gmail.com>
2024-04-24 03:24:02 +0200

    project page started

--- src/pages/project.tsx
+++ src/pages/project.tsx
@@ -1,9 +1,40 @@
+import { useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
+import Header from "../components/header";
+import Footer from "../components/footer";
+
 const Project = () => {
+
+    const [rootCreated, setRootCreated] = useState(false);
+
+    useEffect(() => {
+        if (rootCreated === false) {
+            const headerRoot = document.getElementById('header') as HTMLElement;
+            const footerRoot = document.getElementById('footer');
+    
+            if (headerRoot) {
+                createRoot(headerRoot!).render(<Header />);
+            }
+    
+            if (footerRoot) {
+                createRoot(footerRoot!).render(<Footer />);
+            }
+
+            setRootCreated(true);
+        }
+    }, []);
+
     return (
-        <div>
-            <h1>Project</h1>
-        </div>
-    )
+        <>
+            <header>
+                <div id='header'></div>
+            </header>
+
+            <footer>
+                <div id='footer'></div>
+            </footer>
+        </>
+    );
 }
 
 export default Project;
