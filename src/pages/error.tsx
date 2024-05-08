b7890b0b2316abc72264c9426fd857104a07239c
Elvis <elvishenry2402@gmail.com>
2024-05-08 18:38:00 +0200

    404 file

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -4,12 +4,20 @@ function ErrorPage() {
   
   return (
     <div id='error-body'>
-        <h1>Game Over</h1>
-        <h2>404 Error</h2>
+      <div>
 
-      <a href="/">
-        <h2>Click here to restart</h2>
-      </a>
+        <h1>Game Over</h1>
+        <h2>404 - Error</h2>
+      
+      </div>
+
+      <div>
+
+        <a href="/">
+          <h2>Click here to restart</h2>
+        </a>
+      
+      </div>
       
     </div>
   );
