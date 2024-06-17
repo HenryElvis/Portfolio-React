79419db08caa69a097a00460a4891f808903c924
Elvis Henry <elvishenry2402@gmail.com>
2024-06-17 14:07:57 +0200

    remove marging from top on error page

--- src/pages/error.tsx
+++ src/pages/error.tsx
@@ -1,25 +1,32 @@
+import Cmd from '../components/cmd-components/cmd';
+import CmdButton from '../components/cmd-components/cmd-button';
 import '/src/styles/pages/error.scss';
 
 function ErrorPage() {
   
   return (
-    <div id='error-body'>
-      <div>
+    <>
+      <CmdButton />
+      <Cmd />
+      
+      <div id='error-body'>
+        <div>
 
-        <h1>Game Over</h1>
-        <h2>404 - Error</h2>
+          <h1>Game Over</h1>
+          <h2>404 - Error</h2>
+        
+        </div>
+
+        <div>
+
+          <a href="/">
+            <h2>Click here to restart</h2>
+          </a>
+        
+        </div>
       
       </div>
-
-      <div>
-
-        <a href="/">
-          <h2>Click here to restart</h2>
-        </a>
-      
-      </div>
-      
-    </div>
+    </>
   );
 }
 
