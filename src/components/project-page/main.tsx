31d673a21463291bc98458c35358b5e08459591a
Elvis <elvishenry2402@gmail.com>
2024-06-20 23:42:57 +0200

    fetching ragoon's rumble title data

--- src/components/project-page/main.tsx
+++ src/components/project-page/main.tsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from "react";
 import Presentation from "./presentation";
 
+interface MainProps
+{
+    title: string;
+}
+
 const Main = () => {
+    const [itemData, setItemData] = useState<MainProps | null>(null);
+
+    const dataToFecth = sessionStorage.getItem("dataToFetch");
+    sessionStorage.removeItem("dataToFetch");
+
+    useEffect(() =>
+    {
+        if (!dataToFecth) return;
+
+        fetch(dataToFecth)
+        .then((response) =>
+        {
+            if (!response.ok)
+                throw new Error("Network response was not ok");
+
+            return response.json();
+        })
+        .then ((data) =>
+        {
+            setItemData({title : data.title});
+        })
+        .catch((error: any) => {
+            console.error("Error fetching item data:", error.message);
+        });
+        
+    }, []);
 
     return (
         <main>
-            <h1>Title</h1>
+            <h1>{itemData?.title}</h1>
             <Presentation />
         </main>
     );
