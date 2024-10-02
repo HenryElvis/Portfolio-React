8689293feed00c5bbab9d3bb42e28241cec177a9
Elvis <elvishenry2402@gmail.com>
2024-10-03 01:25:09 +0200

    adding gif to tell some project page is still under construction

--- src/components/project-page/presentation.tsx
+++ src/components/project-page/presentation.tsx
@@ -6,6 +6,7 @@ import DownloadableButton from './DownloadableButton';
 
 const Presentation = () => {
     const [data, /*setData*/] = useState<string>("");
+    const [isPageDone, setIsPageDone] = useState({ PageDone: false});
     const [projectVideo, setProjectVideo] = useState({ hasVideo: false, videoSrc: "" });
     const [blocPres1, setBlocPresentation1] = useState({ title : "", description : "", image1 : "", image2 : "", alt1 : "", alt2 : "" });
     const [blocPres2, setBlocPresentation2] = useState({ title : "", description : "", image1 : "", image2 : "", alt1 : "", alt2 : "" });
@@ -31,6 +32,7 @@ const Presentation = () => {
         })
         .then((data) =>
         {
+            setIsPageDone(data.pageDone);
             setBlocPresentation1({ title: data.bloc1.title, description: data.bloc1.description, image1: data.bloc1.image1, image2: data.bloc1.image2, alt1: data.bloc1.alt1, alt2: data.bloc1.alt2 });
             setBlocPresentation2({ title: data.bloc2.title, description: data.bloc2.description, image1: data.bloc2.image1, image2: data.bloc2.image2, alt1: data.bloc2.alt1, alt2: data.bloc2.alt2 });
             setBlocPresentation3({ title: data.bloc3.title, description: data.bloc3.description, image1: data.bloc3.image1, image2: data.bloc3.image2, alt1: data.bloc3.alt1, alt2: data.bloc3.alt2 });
@@ -43,17 +45,30 @@ const Presentation = () => {
 
     }, [data]);
 
-    return (
-        <>
-            <BlocPresentation blocPresentation={blocPres1}/>
-            <BlocPresentation blocPresentation={blocPres2}/>
-            <BlocPresentation blocPresentation={blocPres3}/>
+    if (!isPageDone)
+    {
+        return (
+            <>
+                <iframe src="https://giphy.com/embed/5huayaHvn5KSkmWZet" width="480" height="480" style={{ border: "none" }} allowFullScreen></iframe><p><a href="https://giphy.com/gifs/lol-wtf-work-5huayaHvn5KSkmWZet">via GIPHY</a></p>
+            </>
+        )
+    }
+    else
+    {
+        return (
+            <>
+                <BlocPresentation blocPresentation={blocPres1}/>
+                    <BlocPresentation blocPresentation={blocPres2}/>
+                    <BlocPresentation blocPresentation={blocPres3}/>
+            
+                    <Video video={projectVideo} />
+            
+                <DownloadableButton dowloadSettings={downloadableSettings} />
+            </>
+        );
+    }
 
-            <Video video={projectVideo} />
 
-            <DownloadableButton dowloadSettings={downloadableSettings} />
-        </>
-    );
 };
 
 export default Presentation;
