72d67d49bc32691dac4d1a639a68111e50911cac
Elvis <elvishenry2402@gmail.com>
2025-02-21 15:33:10 +0100

    adding tower defense project

--- src/components/project-components/project-containers.tsx
+++ src/components/project-components/project-containers.tsx
@@ -8,10 +8,10 @@ const ProjectContainers = () => {
         <div className="project">
             {/* <ProjectItem index={1} /> */}
             {/* <ProjectItem index={2} /> */}
-            {/* <ProjectItem index={3} /> */}
             <ProjectItem index={4} />
             <ProjectItem index={5} />
             <ProjectItem index={6} />
+            <ProjectItem index={3} />
             <ProjectItem index={7} />
             <ProjectItem index={8} />
             <ProjectItem index={9} />
